432adb1858a99fa738a5308ae727f5c1d0edc6a2
Parker Holt <cubman3134@gmail.com>
2019-04-10 11:48:55 -0400

    Update server.py

--- server/server.py
+++ server/server.py
@@ -1,17 +1,42 @@
-# on each iteration of server thread code do:
-#
-#   check for new instructions from client
-#       if got new data, update strategies/metadata
-#
-#   get stock data from AlphaVantage
-#   
-#   process stock data with strategies to determine buy/sell orders
-#   
-#   invoke Robinhood API to execute trades
-#   
-#   return data to client (JSON Export)
-#   
-#   log data
-#
-# end
+import socket
+from _thread import *
+import threading
 
+print_lock = threading.Lock()
+
+def threaded(c):
+    data = None
+    #c.setblocking(0)
+    while True:
+        #print("oy")
+        try:
+            data = c.recv(1024)
+            data = data[::-1]
+            c.send(data)
+            c.send('cool'.encode())
+            print("Sent some data...")
+        except socket.error:
+            pass
+        else:
+            print("error")
+
+        #if not data:
+        #    print('Bye')
+        #    break
+    c.close()
+
+def Main():
+    host = ""
+    port = 12345
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((host, port))
+    print("socket binded to post", port)
+    s.listen(5)
+    print("socket is listening")
+    while True:
+        c, addr = s.accept()
+        print('Connected to :', addr[0], ':', addr[1])
+        start_new_thread(threaded, (c,))
+    s.close()
+if __name__ == '__main__':
+    Main()
