848b37ef6ea58d28541ee6979f1cdd519c119bfa
David Clevenger <dclevenger00@gmail.com>
2019-04-22 12:50:45 -0400

    Fleshing out server.

--- server/server.py
+++ server/server.py
@@ -1,12 +1,67 @@
 import socket
 from _thread import *
 import threading
+import signal
 
 print_lock = threading.Lock()
 
+port = 12345
+
+class Connection:
+    def __init__(self, username, comm, auth, refresh, code):
+        self.username = username
+        self.comm = comm
+        self.token = auth
+        self.refresh = refresh
+        self.code = code
+
+class Server:
+    def __init__(self):
+        global port
+        self.connections = {}  # connected clients { username -> socket }
+        self.pool = []
+        self.master_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.master_sock.bind(('',port))
+
+        # setup signal handlers
+        signal.signal(signal.SIGTERM, self.close)
+        signal.signal(signal.SIGTRAP, signal.SIG_IGN)
+        signal.signal(signal.SIGINT, signal.SIG_IGN)
+
+    def close(self):
+        self.master_sock.close()
+
+    def listen(self):
+        self.master_sock.listen()
+
+        while True:
+            client, _ = self.master_sock.accept()
+            msg = client.recv(1024)
+            fields = msg.split(' ')
+            username = fields[1]
+            code = client.recv(4096)
+            curr = Connection(username, client, fields[2], fields[3], code)
+            self.connections[username] = curr
+
+            tmp = threading.Thread()
+
+            # return success to signal
+            client.send('OK'.encode())
+
+    def handle_client(self, username):
+        connection = self.connections[username]
+
+        # WIP: DEC
+
+
+
+
+def main():
+    server = Server()
+    server.listen()
+
 def threaded(c):
     data = None
-    #c.setblocking(0)
     while True:
         #print("oy")
         try:
@@ -38,5 +93,7 @@ def Main():
         print('Connected to :', addr[0], ':', addr[1])
         start_new_thread(threaded, (c,))
     s.close()
+
+
 if __name__ == '__main__':
-    Main()
+    main()
